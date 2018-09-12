8cda922659db5b15fa5896a690c293b125d40ca4
Drow Ranger <34815373+drow-ranger@users.noreply.github.com>
2018-09-13 06:19:41 +0700

    merapikan

--- app/src/main/java/com/deonico/footballclub2gdk_fight/MainAdapter.kt
+++ app/src/main/java/com/deonico/footballclub2gdk_fight/MainAdapter.kt
@@ -5,7 +5,13 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.match_detail.*
 import kotlinx.android.synthetic.main.match_list.view.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlin.coroutines.experimental.coroutineContext
 
 class MainAdapter(private val context: Context, private val events: List<Event>, private val listener: (Event) -> Unit) : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -24,16 +30,15 @@ class MainAdapter(private val context: Context, private val events: List<Event>,
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bindItem(event: Event, listener: (Event) -> Unit) {
-
             itemView.dateMatch.text = event.eventDate
             itemView.homeTeam.text = event.strHomeTeam
             itemView.homeScore.text = event.intHomeScore
             itemView.awayTeam.text = event.strAwayTeam
             itemView.awayScore.text = event.intAwayScore
+            Glide.with(itemView.context).load(event.strHomeTeam).into(itemView.homeLogo)
             itemView.setOnClickListener {
                 listener(event)
             }
-
         }
     }
 }
